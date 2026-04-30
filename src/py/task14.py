9c55f79cd6dc1744a56e5abba7a635188d12a95c
Robert <robertmel1710@gmail.com>
2026-04-29 23:07:08 -0600

    feat: Solucion ejercicio 14

--- src/py/task14.py
+++ src/py/task14.py
@@ -1,1 +1,40 @@
 #Archivo para trabajar en cada sección del módulo
+#Ejercicio 14 
+
+import numpy as np
+
+def cross_entropy_loss(probs, targets):
+
+    N = len(targets)
+
+    correct_probs = probs[np.arange(N), targets]
+
+    eps = 1e-9
+
+    log_probs = np.log(correct_probs + eps)
+
+    loss = -np.mean(log_probs)
+
+    return loss
+# %%
+
+# %%
+# Ejemplo 1
+probs = np.array([[0.1, 0.9],
+                  [0.8, 0.2]])
+
+targets = np.array([1, 0])
+
+print(cross_entropy_loss(probs, targets))
+
+# %%
+
+# %%
+# Ejemplo 2
+probs = np.array([[0.9, 0.1],
+                  [0.2, 0.8]])
+
+targets = np.array([1, 0])
+
+print(cross_entropy_loss(probs, targets))
+# %%
